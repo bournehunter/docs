044b7b545b285caea9958ce603e697447e0bcfa1
Horacio Rilo <76416032+bournehunter@users.noreply.github.com>
2024-08-27 02:17:32 -0500

    Create theme.config.jsx

---
+++ theme.config.jsx
@@ -0,0 +1,10 @@
+export default {
+  logo: <span>Boop SMS Business Cards</span>,
+  project: {
+    link: 'https://github.com/bournehunter/docs'
+  },
+  docsRepositoryBase: 'https://github.com/bournehunter/docs/tree/main',
+  footer: {
+    text: '© 2024 Boop SMS Business Cards'
+  }
+}
